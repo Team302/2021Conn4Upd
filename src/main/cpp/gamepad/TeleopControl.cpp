ca80ec2892c4e7a84fd7822f558665844d9ee23f
notcharlie2026 <118229690+notcharlie2026@users.noreply.github.com>
2023-08-28 20:53:17 -0400

    button map changes
    made arm controls more like 2022 connect 4 bot

--- src/main/cpp/gamepad/TeleopControl.cpp
+++ src/main/cpp/gamepad/TeleopControl.cpp
@@ -101,9 +101,9 @@ TeleopControl::TeleopControl() : m_axisIDs(),
 		m_buttonIDs[EXPEL]                  = IDragonGamePad::LEFT_BUMPER;
 
 		m_controllerIndex[ROTATE_ARM_UP]    = ctrlNo;
-		m_buttonIDs[ROTATE_ARM_UP]          = IDragonGamePad::Y_BUTTON;
+		m_buttonIDs[ROTATE_ARM_UP]          = IDragonGamePad::RIGHT_TRIGGER;
 		m_controllerIndex[ROTATE_ARM_DOWN]  = ctrlNo;
-		m_buttonIDs[ROTATE_ARM_DOWN]        = IDragonGamePad::A_BUTTON;
+		m_buttonIDs[ROTATE_ARM_DOWN]        = IDragonGamePad::LEFT_TRIGGER;
 
 		m_controllerIndex[RELEASE]          = ctrlNo;
 		m_buttonIDs[RELEASE]                = IDragonGamePad::B_BUTTON;
